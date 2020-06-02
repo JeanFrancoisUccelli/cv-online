ecd5a32d953c4c426b2aa73699e84c806741d820
jeanfrancoisuccelli <jeanfrancoisuccelli@gmail.com>
2020-06-02 10:19:20 +0200

    update cv links

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,33 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class ContactUs extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <p className="lead">
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">
               Feel free to contact me for any work or suggestions below
-              </p>
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <h4>
+                <a
+                  href="https://www.linkedin.com/in/jeff-uccelli"
+                  target="_blank"
+                >
+                  Linked in :{resumeData.linkedinId}
+                </a>
+              </h4>
+              <h4>
+                <a href="mailto:jeanfrancoisuccelli@gmail.com">
+                  Mail : Jeff Uccelli
+                </a>
+              </h4>
+              <h4>
+                <a href="https://drive.google.com/file/d/1DQg5xOt5-a3lf8iwgnN79Ia-7aGIxUGj/view?usp=sharing">
+                  Mon CV en Francais au format pdf
+                </a>
+              </h4>
+              <h4>
+                <a href="https://drive.google.com/file/d/1D5o6UD0n5d4Xoz-z22ma4hq-dFZi6eu1/view?usp=sharing">
+                  My CV in English in pdf format
+                </a>
+              </h4>
             </div>
-          </div>
-          <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-              <h4><a href='https://www.linkedin.com/in/jeff-uccelli' target='_blank'>Linked in :
-                  {resumeData.linkedinId}                  
-                </a></h4>
-              <h4><a href='mailto:jeanfrancoisuccelli@gmail.com'>Mail :
-                  Jeff Uccelli
-              </a></h4>
-              <h4><a href='https://drive.google.com/file/d/1IggHGIu7S4-L-WwAdlhYLJHzkZ5A5kK6/view?usp=sharing'>Mon CV en Francais au format pdf
-              </a></h4>
-              <h4><a href='https://drive.google.com/file/d/1kV7MMqwnxysKn53ARDAsoAb2VYx4_4ft/view?usp=sharing'>My CV in English in pdf format
-              </a></h4>
-              </div>
-            </aside>
-          </div>
-        </section>
-        );
+          </aside>
+        </div>
+      </section>
+    );
   }
 }
