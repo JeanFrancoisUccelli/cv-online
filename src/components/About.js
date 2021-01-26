c6c790874f69498827e691640d70f7ddb8aeee29
JeanFrancoisUccelli <jeanfrancoisuccelli@gmail.com>
2021-01-26 16:28:30 +0100

    change profile pic to iphone simulator

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default class About extends Component {
     return <section id="about">
         <div className="row">
           <div className="three columns">
-            <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+            <img className="profile-pic" src="images/photo_portrait.png" alt="" />
           </div>
 
           <div className="nine columns main-col">
