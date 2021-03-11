48e60111612a274bf854ab7552d67a31294d35aa
JeanFrancoisUccelli <jeanfrancoisuccelli@gmail.com>
2021-03-11 17:26:38 +0100

    add link to new french cv

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -37,7 +37,7 @@ export default class ContactUs extends Component {
                 > */}
                 <ReactGA.OutboundLink
                   eventLabel="clickToFrenchCV"
-                  to="https://drive.google.com/file/d/1D5o6UD0n5d4Xoz-z22ma4hq-dFZi6eu1/view?usp=sharing"
+                  to="https://drive.google.com/file/d/1gvTbcAbegmfhvWHXbHcJIYNS_2IarkYR/view?usp=sharing"
                   target="_blank"
                 >
                   Mon CV en Francais au format pdf
